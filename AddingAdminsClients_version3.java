1a4651a3bf9d4440da810bb29a6c4d3f55d4cb23
Bodhith <34181215+Bodhith@users.noreply.github.com>
2018-09-16 19:01:40 +0530

    Create AddingAdminsClients_version3.java

---
+++ AddingAdminsClients_version3.java
@@ -0,0 +1,216 @@
+package com.bodhith.smartvillages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
+
+
+////////////////////////////////////////////// MAIN ///////////////////////////////////////////////////////
+
+public class MainActivity extends AppCompatActivity
+{
+
+    private long NumberOfAdmins , NumberOfClients ;
+
+    private EditText Username , Password ;
+
+    private Button Submit ;
+
+    private DatabaseReference DataBase = FirebaseDatabase.getInstance().getReference() ;
+
+    private DatabaseReference DataBaseTemp , Admins , Clients ;
+
+    private DataSnapshot SnapShot ;
+
+    private String username , password ;
+
+    private Users user = new Users() ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        Toast.makeText(getApplicationContext(), "Hello", Toast.LENGTH_SHORT).show();
+
+        Username =  findViewById(R.id.Username);
+
+        Password =  findViewById(R.id.Password);
+
+        DataBase.child("Admins").addValueEventListener(new AddAdminValueEventListener());
+
+        DataBase.child("Clients").addValueEventListener(new AddClientValueEventListener());
+
+        Submit =  findViewById(R.id.Submit);
+    }
+
+//////////////////////////////////////////////// END MAIN //////////////////////////////////////////////////
+
+//////////////////////////////////////////////// LISTENERS /////////////////////////////////////////////////
+
+    /*                                           ADMIN                                            */
+    class AddAdminValueEventListener implements ValueEventListener
+    {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot)
+        {
+            SnapShot = dataSnapshot.child("CurrentAdminsCount");
+
+            NumberOfAdmins = Long.parseLong(SnapShot.getValue().toString());
+
+            Log.d("snap","2");
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError)
+        {
+            Log.d("snap","2.2");
+        }
+    }
+    /*                                          END ADMIN                                         */
+
+    /*                                         START CLIENT                                       */
+    class AddClientValueEventListener implements ValueEventListener
+    {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot)
+        {
+            SnapShot = dataSnapshot.child("CurrentClientsCount");
+
+            NumberOfClients = Long.parseLong(SnapShot.getValue().toString());
+
+            Log.d("snap","2");
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError)
+        {
+            Log.d("snap","2.2");
+        }
+    }
+
+    /*                                          END CLIENT                                        */
+
+/////////////////////////////////////////////// END LISTENERS /////////////////////////////////////////////
+
+    public void Login(View v)
+    {
+        username = Username.getText().toString();
+
+        password = Password.getText().toString();
+
+        final String Check = CheckLogin(username,password);
+
+        if( Check.equals("Admin") )
+        {
+            AddAdmins(username,password);
+
+            Log.d("snap","3");
+
+            //    Toast.makeText(this, "Admin Interface", Toast.LENGTH_SHORT).show();
+        }
+        else if( Check.equals("Client") )
+        {
+            AddClients(username,password);
+
+            Log.d("snap","3");
+
+            //Toast.makeText(this, "Client Interface", Toast.LENGTH_SHORT).show();
+        }
+        else if( Check.equals("Fasaak") )
+        {
+            Toast.makeText(this, "Login Unsuccessful", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public String CheckLogin(final String username,final String password)
+    {
+        return "Admin";
+    }
+
+    public void AddAdmins(String Username,String Password)
+    {
+        DataBaseTemp = DataBase.child("Admins");
+
+        user.setUsername(Username);
+
+        user.setPassword(Password);
+
+        DataBaseTemp.child("admin"+Long.toString(++NumberOfAdmins)).setValue(user);
+
+        DataBaseTemp.child("CurrentAdminsCount").setValue(NumberOfAdmins);
+
+        DataBaseTemp.child("admin"+Long.toString(NumberOfAdmins)).child("status").setValue("Active");
+
+        Log.d("snap","4");
+    }
+
+    public void AddClients(String Username,String Password)
+    {
+        DataBaseTemp = DataBase.child("Clients");
+
+        user.setUsername(Username);
+
+        user.setPassword(Password);
+
+        DataBaseTemp.child("client"+Long.toString(++NumberOfClients)).setValue(user);
+
+        DataBaseTemp.child("CurrentClientsCount").setValue(NumberOfClients);
+
+        DataBaseTemp.child("client"+Long.toString(NumberOfClients)).child("status").setValue("Active");
+
+        Log.d("snap","4");
+    }
+
+    public void SetUsername(String Username)
+    {
+
+        user.setUsername(Username);
+    }
+
+    public void SetPassword(String Password)
+    {
+        user.setPassword(Password);
+    }
+
+    /* TO DO
+
+    Method Complete cheystae admin count aentha mandhi vunnaro telustundhi listeners vadi today
+    ->Debug cheyali
+    ->Event Listeners Nerchukovali
+    ->Ae Even Listener First trigger avthundho check karo
+    ->Search cheyatam nerchukvali
+
+    public long AdminCount()
+    {
+        DataBaseTemp = DataBase;
+        SnapShot = DataBaseTemp.child("CurrentAdminCount");
+        long AdminCount = SnapShot.child("Admins").getChildrenCount();
+        Toast.makeText(this, ""+AdminCount, Toast.LENGTH_SHORT).show();
+        return AdminCount;
+    }*/
+
+/////////////////////////////////////////  MISC ////////////////////////////////////////////////////
+
+
+
+/////////////////////////////////////////// END MISC ///////////////////////////////////////////////
+}
